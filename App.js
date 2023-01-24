49fe2c5b4d31117ee28ec72641e31c48b45659fe
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-24 16:27:16 -0500

    installed necessary packages and configured app.json for building dev client in Expo.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./StackNavigator";
+// import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
 	return (
